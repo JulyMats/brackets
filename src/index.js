89247951ebe9f6326ca691dfea595bc426d968aa
Matsukevich Yuliya <july.matsukevich@mail.ru>
2023-10-09 20:54:00 +0300

    add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let isValid = true;
+
+  for (let i = 0; i < str.length; i += 1) {
+    bracketsConfig.map(e => {
+      if(str[i] === e[0]) {
+        if(e[0] === e[1] && stack[stack.length - 1] === e[0]) {
+          stack.pop();
+        } else {
+          stack.push(e[1]);
+        }
+      } else if(str[i] === e[1]) {
+        if(stack[stack.length - 1] === e[1]) {
+          stack.pop();
+        } else {
+          isValid = false;
+        }
+      }
+    });
+    
+    if(!isValid) {
+      return isValid;
+    }
+  }
+  return !stack.length;
 }
